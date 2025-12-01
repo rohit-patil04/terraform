dd9357832cbb88f4d85f5cba127d7961c81e32a3
Rohit Patil <167469135+rohit-patil04@users.noreply.github.com>
2025-12-01 17:12:27 +0530

    Update main.tf

--- Day 3 /main.tf
+++ Day 3 /main.tf
@@ -1,1 +1,13 @@
+#creating a S3 bucket 
 
+resource "aws_s3_bucket" "aws_s3_bucket" {
+  bucket = "my-tf-test-bucket-12345"
+
+
+  tags = {
+    Name        = "My S3 bucket"
+    Environment = "Testing Env"
+  }
+
+
+}
