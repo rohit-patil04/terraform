9bd4fbf5ec739df2d5d750d81cf2ecf4dad7f599
Rohit Patil <167469135+rohit-patil04@users.noreply.github.com>
2025-12-01 16:42:58 +0530

    Add AWS provider configuration in Provider.tf

--- Day 2/Provider.tf
+++ Day 2/Provider.tf
@@ -1,1 +1,14 @@
+#provider configuration for AWS
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
 
+# Configure the default AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
