58158321dea69b59bec11add18b1f981c13dd79f
Rohit Patil <167469135+rohit-patil04@users.noreply.github.com>
2025-12-02 17:40:28 +0530

    Update backend.tf

--- Day 4 /backend.tf
+++ Day 4 /backend.tf
@@ -1,1 +1,10 @@
-
+terraform {
+        backend "s3" {
+            bucket = "my-tf-test-buroniiiiiicket12345backennstategfile"
+            key    = "dev/terraform.tfstate"
+            region = "ap-southeast-1"
+            encrypt = true
+            use_lockfile = true
+            
+        }
+        }
