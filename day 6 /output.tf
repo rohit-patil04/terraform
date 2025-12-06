e9a08952297ee118828109be48eb69c85514f537
Rohit Patil <167469135+rohit-patil04@users.noreply.github.com>
2025-12-06 17:39:29 +0530

    Add output variables for S3, VPC, and EC2

--- day 6 /output.tf
+++ day 6 /output.tf
@@ -1,1 +1,21 @@
+##Output Variables
 
+#output for S3 bucket name
+output "s3_bucket_name" {
+    description = "The name of the S3 bucket"
+    value       = aws_s3_bucket.test_bucket.bucket
+
+}
+#output for VPC ID
+output "vpc_id" {
+    description = "The ID of the VPC"
+    value       = aws_vpc.test_vpc.id
+
+}
+
+    #output for EC2 instance ID
+  output "ec2_instance_id" {  
+        description = "The ID of the EC2 instance"
+      value       = aws_instance.test.id
+
+    }
