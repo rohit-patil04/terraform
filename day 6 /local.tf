c23543ec6d489e384247df5cbf25c816ba391d52
Rohit Patil <167469135+rohit-patil04@users.noreply.github.com>
2025-12-06 17:37:52 +0530

    Define local variables for resource naming
    Added local variables for resource naming.

--- day 6 /local.tf
+++ day 6 /local.tf
@@ -1,1 +1,11 @@
+    ##Local Variables
+
+#local values for resource naming
+ 
+locals {
+    bucket_name = "my-unique-${var.Environment}-bucket-123456"
+    vpc_name    = "${var.Environment}-VPC"
+    instance_name = "${var.Environment}-Instance"
+  
+}
 
