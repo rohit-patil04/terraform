29f8f665720540d8db5f243b2cb1bd85d39f5655
Rohit Patil <167469135+rohit-patil04@users.noreply.github.com>
2025-12-06 17:36:54 +0530

    add a backend file

--- day 6 /backend.tf
+++ day 6 /backend.tf
@@ -1,1 +1,11 @@
 
+terraform {
+        backend "s3" {
+            bucket = "my-tf-test-buroniiiiiicket12345backennstategfile"
+            key    = "dev/terraform.tfstate"
+            region = "ap-southeast-1"
+            encrypt = true
+            use_lockfile = true
+            
+        }
+        }
